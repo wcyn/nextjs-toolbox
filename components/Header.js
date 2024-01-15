c19306257cf677e5bd22c7b5a8a99fc9d111413b
Cynthia Wasonga <wasonga.cynthia@gmail.com>
2024-01-15 10:00:29 +0300

    Partially format Header text

--- components/Header.js
+++ components/Header.js
@@ -19,7 +19,7 @@ export default function Header({ title }) {
         <a href="https://github.com/netlify-templates/nextjs-toolbox">
           https://github.com/netlify-templates/nextjs-toolbox
         </a>
-        ! Happy coding!
+        ! <pre>Happy coding!</pre>
       </p>
     </>
   );
